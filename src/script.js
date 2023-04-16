a9c8e49ea5b2169234fd17787434c44581421d59
kathleen-mayfield <kathleenkmayfield@gmail.com>
2023-04-15 20:48:01 -0600

    Showing the current temperature, description, humidity, wind-speed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+
+  descriptionElement.innerHTML = response.data.condition.description;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "teac3ae1388oef3f802de875490b062f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
