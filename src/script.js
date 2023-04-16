878642e051a564fdd7b6630ed15a9077dba03adf
kathleen-mayfield <kathleenkmayfield@gmail.com>
2023-04-16 14:20:37 -0600

    showing current date and time

--- src/script.js
+++ src/script.js
@@ -1,20 +1,38 @@
+function formatDate(timestamp) {
+  let currentDate = new Date();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
-
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
 
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate();
 }
 
 let apiKey = "teac3ae1388oef3f802de875490b062f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
